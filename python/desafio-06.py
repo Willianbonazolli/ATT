e23bcebf2276cefc31b5de18371b645aabcf3085
Willian Bonazzoli <118841428+Willianbonazolli@users.noreply.github.com>
2026-03-27 06:17:01 -0300

    resolução de exercícios

---
+++ python/desafio-06.py
@@ -0,0 +1,9 @@
+n1 = int(input("Digite um numero: "))
+
+n2 = n1 * 2
+n3 = n1 * 3
+n4 = n1 ** 0.5
+
+print("Dobro do seu numero {}".format(n2))
+print("Triplo do seu numero {}".format(n3))
+print("A raiz quadrada do seu numero {}".format(n4))
