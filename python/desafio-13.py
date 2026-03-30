a401ffdf84f1b17fa9b03bedda06a7ea811d389e
Willian Bonazzoli <118841428+Willianbonazolli@users.noreply.github.com>
2026-03-30 10:14:29 -0300

    atividades

---
+++ python/desafio-13.py
@@ -0,0 +1,6 @@
+print("Qual o seu salário? ")
+salario = float(input("R$: "))
+
+aumento = salario * 1.15
+
+print("Com aumento de 15% seu salário fica R$:{:.2f}".format(aumento))
---
+++ python/desafio-15.py
@@ -0,0 +1,5 @@
+tempc = float(input("Informe a temperatura em C: "))
+
+tempf = (tempc * 1.8) + 32
+
+print(" Essa é sua temperatura em F: {:.1f}".format(tempf))
