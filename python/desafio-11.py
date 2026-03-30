a7dd3e9eb95ab0ac8f9ef0d37aa518a72f4f43a5
Willian Bonazzoli <118841428+Willianbonazolli@users.noreply.github.com>
2026-03-30 09:49:39 -0300

    resolução de atividades

---
+++ python/desafio-11.py
@@ -0,0 +1,9 @@
+altura = float(input("Qual a largura da sua parede ?"))
+largura = float(input("Qual a altura da sua parede ?"))
+
+area = altura * largura
+
+tinta = area / 2
+
+print("A quantidade necessária de tinta para pintar a parede é de {:.2f} litros".format(tinta))
+
